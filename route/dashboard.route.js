bfc733e3edaf6e8bda33287d4f71428ca3c1ffc1
Hieu <hieu@weventure.global>
2020-06-20 12:55:07 +0700

    manager category

--- route/dashboard.route.js
+++ route/dashboard.route.js
@@ -3,8 +3,16 @@ const router = express.Router()
 const auth = require('../middlewares/auth.mdw')
 const userModel = require('../model/users.model')
 
+////////////ADMIN//////////////
+
 router.get('/', (req, res) => {
     res.render('dashboard/admin', {
+        layout: false,
+    })
+})
+
+router.get('/category', (req, res) => {
+    res.render('dashboard/category', {
         layout: false,
     })
 })
