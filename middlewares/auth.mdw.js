d4cb01aae8a7fd23e83a1ee24aafaa2f9a25ac78
Hieu <hieu@weventure.global>
2020-06-24 14:01:47 +0700

    fix auth

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -14,19 +14,19 @@ module.exports = {
     next()
   },
   isAdmin: (req, res, next) => {
-    if (req.session.authUser != null && req.session.authUser == userModel.Admin) {
+    if (req.session.isAuthenticated && req.session.authUser == userModel.Admin) {
       next()
     }
     return res.redirect(`../index`)
   },
   isSubscriber: (req, res, next) => {
-    if (req.session.authUser != null && req.session.authUser != userModel.Subscriber) {
+    if (req.session.isAuthenticated && req.session.authUser == userModel.Subscriber) {
       next()
     }
     return res.redirect(`../index`)
   },
   isWriter: (req, res, next) => {
-    if (req.session.authUser != null && req.session.authUser != userModel.Writer) {
+    if (req.session.isAuthenticated && req.session.authUser == userModel.Writer) {
       next()
     }
     return res.redirect(`../index`)
